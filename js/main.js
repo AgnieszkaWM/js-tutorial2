36508be6866dc07fa59870754070f8681ea9f14e
Agnieszka W.M <aniesw@poczta.onet.pl>
2018-01-09 13:05:12 +0100

    Create index.html and js file

---
+++ js/main.js
@@ -0,0 +1,13 @@
+/*
+var name = prompt ('Enter your name');
+alert('Hello, ' + name);
+console.log('Hello, ' + name);
+*/
+
+//var variable = 'test';
+//console.log(variable);
+
+var a = 2,
+	h = 9,
+	triangleArea = a*h/2;
+console.log('Trangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea + '.');
