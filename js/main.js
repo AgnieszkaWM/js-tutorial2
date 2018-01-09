2015ba39e7f62f4c656c45d4bb1a525b55976026
Agnieszka W.M <aniesw@poczta.onet.pl>
2018-01-09 14:19:00 +0100

    Add prompt function to variables

--- js/main.js
+++ js/main.js
@@ -7,7 +7,11 @@ console.log('Hello, ' + name);
 //var variable = 'test';
 //console.log(variable);
 
-var a = 2,
-	h = 9,
-	triangleArea = a*h/2;
-console.log('Trangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea + '.');
+
+
+var a = prompt('Enter a base of triangle');
+var h = prompt('Enter a height of triangle');
+var triangleArea = a*h/2;
+alert('Triangle field with base: ' + a + ' and height: ' + h + ' is equal to: ' + triangleArea + '.')
+
+console.log('Triangle field with base: ' + a + ' and height: ' + h + ' is equal to: ' + triangleArea + '.');
